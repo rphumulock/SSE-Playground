d9f94609b1f122f69207cf218dc0578cbe2873d3
rphumulock <rphumulock@gmail.com>
2024-02-21 12:41:51 -0500

    First!

---
+++ javascript/index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const favicon = require('serve-favicon');
+const path = require('path');
+
+const app = express();
+const http = require('http').Server(app);
+
+// Serve the favicon
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+// Serve static files from the public directory
+app.use(express.static('public'));
+
+// Route for serving the index.html file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+// Server-Sent Events endpoint
+app.get('/events', (req, res) => {
+  res.setHeader('Content-Type', 'text/event-stream');
+  res.setHeader('Cache-Control', 'no-cache');
+  res.setHeader('Connection', 'keep-alive');
+
+  let eventCount = 0;
+
+  const intervalId = setInterval(() => {
+    if (eventCount >= 20) {
+      const message = `data: All done!\n\n`;
+      res.write(message);
+      res.end(); // Close the connection
+    } else {
+      const message = `data: ${new Date().toLocaleTimeString()}\n\n`;
+      res.write(message);
+      eventCount++;
+    }
+  }, 1000);
+
+  // Clean up on client disconnect
+  req.on('close', () => {
+    clearInterval(intervalId);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
+});
